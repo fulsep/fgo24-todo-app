dae078fcee43c2c342d49403653ab2261450012c
HN721 <hosea1422@gmail.com>
2025-05-22 22:13:54 +0700

    fix: fix bug on button add new task using dispatchEvent to handle submit data

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,21 +1,33 @@
-import TaskItem from './TaskItem'
-import { useSelector } from 'react-redux'
+import TaskItem from "./TaskItem";
+import { useSelector } from "react-redux";
 
-function TaskList({inputRef}) {
-  const tasks = useSelector(state => state.todos.list)
+function TaskList({ inputRef }) {
+  const tasks = useSelector((state) => state.todos.list);
   return (
     <>
-      {tasks.map((task,idx)=>{
-        return (
-          <TaskItem {...task} key={`task-${idx}`} />
-        )
+      {tasks.map((task, idx) => {
+        return <TaskItem {...task} key={`task-${idx}`} />;
       })}
-      {tasks.length === 0 && <div className='p-10 flex flex-col gap-5 justify-center items-center'>
-        <div className='text-3xl'>There is no tasks!</div>
-        <button onClick={()=>inputRef.current.focus()} type='button' className='h-12 px-5 bg-blue-500 cursor-pointer text-white rounded'>Add a new one</button>
-      </div>}
+      {tasks.length === 0 && (
+        <div className="p-10 flex flex-col gap-5 justify-center items-center">
+          <div className="text-3xl">There is no tasks!</div>
+          <button
+            onClick={() => {
+              console.log(inputRef.current);
+              inputRef.current.focus();
+              inputRef.current.form.dispatchEvent(
+                new Event("submit", { cancelable: true, bubbles: true })
+              );
+            }}
+            type="button"
+            className="h-12 px-5 bg-blue-500 cursor-pointer text-white rounded"
+          >
+            Add a new one
+          </button>
+        </div>
+      )}
     </>
-  )
+  );
 }
 
-export default TaskList
+export default TaskList;
