6f954fab1a166764d08adc609304b1c60f1ef801
mdavindarinaldy <153896812+mdavindarinaldy@users.noreply.github.com>
2025-05-23 23:23:44 +0700

    fix: resolve duplicate id bug

--- src/redux/reducers/todos.js
+++ src/redux/reducers/todos.js
@@ -9,7 +9,9 @@ const todos = createSlice({
   initialState,
   reducers: {
     addTask: function(state, action){
-      const id = state.list.length +1
+      let id
+      if (state.list.length===0) {id = state.list.length+1}
+      else {id = state.list[(state.list.length-1)].id+1 }
       state.list.push({
         id,
         taskName: action.payload,
